d4fbf34ea7479118f40650a1014e75230809b057
Daniel Laera <laera.daniel@hotmail.com>
2019-12-24 16:51:31 +0100

    first commit

---
+++ src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -0,0 +1,17 @@
+package com.example.accessingdatajpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/accessingdatajpa/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,64 @@
+package com.example.accessingdatajpa.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.accessingdatajpa.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        "johndoe",
+                        passwordEncoder.encode("password"),
+                        STUDENT.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "paulsmith",
+                        passwordEncoder.encode("password123"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "tomwelling",
+                        passwordEncoder.encode("password12"),
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
+    }
+}
